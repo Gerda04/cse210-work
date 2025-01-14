0f5aaa247259dfe96199108b2da26bd7fa95110b
Gerda04 <amr23001@byui.edu>
2025-01-13 18:29:00 -0700

    Prove 1 + 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the grade percentage?");
+        string percentage = Console.ReadLine();
+        int number = int.Parse(percentage);
+
+        if (number >= 90)
+        {
+            Console.WriteLine("You have an A");
+        }
+        else if (number < 90 && number >= 80)
+        {
+            Console.WriteLine("You have a B");
+        }
+        else if (number < 80 && number >= 70)
+        {
+            Console.WriteLine("You have a C");
+        }
+        else if (number < 70 && number >= 60)
+        {
+            Console.WriteLine("You have a D");
+        }
+        else if (number<60)
+        {
+            Console.Write("You have an F");
+        }
+        else
+        {
+            Console.WriteLine("Something's wrong");
+        }
     }
 }
