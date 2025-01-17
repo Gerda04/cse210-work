37c086c30b003e1b58c807127b60998f6c4c7f74
Gerda04 <amr23001@byui.edu>
2025-01-16 17:40:21 -0700

    prove assignments

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string number = Console.ReadLine();
+        int magic_number = int.Parse(number);
+        int guess = -1;
+
+        while (guess != magic_number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magic_number > guess)
+            {
+                Console.Write("Higher\n");
+            }
+            else if (magic_number < guess)
+            {
+                Console.Write("Lower\n");
+            }
+            else
+            {
+                Console.Write("You guessed it!");
+            }
+        }
+
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.Write("Welcome to the program\n");
+
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Please enter your favorite number ");
+        int number = int.Parse(Console.ReadLine());
+
+        int total =number * number;
+
+        Console.WriteLine($"{name}, the square of your number is {total}");
+
     }
 }
